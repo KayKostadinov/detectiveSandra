15a77e00393b52839a5640abd698cd4541997cbc
Kay <kay@pop-os.localdomain>
2021-01-27 23:36:42 +0200

    initial commit

---
+++ src/Story.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 
+
+function Story() {
+    return (
+        <div>
+            <header class="mb-5">
+                <div class="header">
+                    <h2>Детектив Сандра</h2>
+                </div>
+            </header>
+            <main class="container main">
+                <div class="card mb-4 image-section">
+                    <img src="" alt="" class="img-fluid"></img>
+                </div>
+                <div class="card p-3 text-section">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, nam ducimus. Corrupti cupiditate accusamus, natus eligendi quis impedit illo laborum. Adipisci quidem suscipit ullam delectus voluptatum quasi magni, repellendus exercitationem!</p>
+                </div>
+                <button type="button" class="btn btn-primary btn-large w-100 my-5 text-center"><i class="fas fa-arrow-circle-right"></i></button>
+            </main>
+        </div>
+    )
+}
+
+export default Story
